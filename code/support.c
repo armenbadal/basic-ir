8416ef7eedb3395941f2cd2331b090b8ec05da8c
Armen Badalian <armenbadal@gmail.com>
2014-12-02 18:16:46 +0400

    support library

---
+++ code/support.c
@@ -0,0 +1,63 @@
+
+/*
+BASIC IR լեզվի ներդրված ֆունկցիաների գրադարան։
+*/
+
+extern int scanf(const char*, ...);
+extern int printf(const char*, ...);
+
+/* Ներմուծում */
+int __input_integer__()
+{
+  int val = 0;
+  scanf("%d", &val);
+  return val;
+}
+double __input_double__()
+{
+  double val = 0.0;
+  scanf("%lf", &val);
+  return val;
+}
+_Bool __input_boolean__()
+{
+  return 0;
+}
+
+/* Արտածում */
+void __print_integer__(int val)
+{
+  printf("%d", val);
+}
+void __print_double__(double val)
+{
+  printf("%lf", val);
+}
+void __print_boolean__(_Bool val)
+{
+  printf("%s", val ? "True" : "False");
+}
+
+/* Մաթեմատիկական ֆունկցիաներ ? 
+ Օգտագործել LLVM֊ի ներդրված ֆունկցիաները։ */
+double __f_pow__(double x, double y)
+{
+  return 0.0;
+}
+int __i_pow__(int x, int y)
+{
+  int val = 1;
+  while( y != 0 ) {
+    val *= x;
+    --y;
+  }
+  return val;
+}
+
+/* Մուտքի կետ */
+int main()
+{
+  Main();
+  return 0;
+}
+
