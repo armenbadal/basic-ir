8fe71dab8f844c392c06a317b71b2cd8a52d05c9
Armen Badalian <armenbadal@gmail.com>
2017-05-21 12:58:59 +0400

    Update lexeme.hxx

--- src/lexeme.hxx
+++ src/lexeme.hxx
@@ -5,23 +5,71 @@
 #include <string>
 
 namespace basic {
-  //
-  enum class Token {
-    None
+  ///
+  enum class Token : int {
+    None,
+
+    Real,
+    Text,
+    Identifier,
+
+    Declare,
+    Function,
+    Input,
+    Print,
+    Let,
+    If,
+    Then,
+    ElseIf,
+    Else,
+    While,
+    For,
+    To,
+    Step,
+    Call,
+    End,
+
+    NewLine,
+
+    Eq,
+    Ne,
+    Lt,
+    Le,
+    Gt,
+    Ge,
+
+    LeftPar,
+    RightPar,
+    Comma,
+
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Mod,
+    Pow,
+
+    And,
+    Or,
+    Not,
+
+    Eof
   };
 
-  //
+  ///
   class Lexeme {
   public:
     Token kind = Token::None;
     std::string value = "";
     unsigned int line = 0;
-
+  
   public:
     Lexeme() = default;
-    Lexeme( Token k, const std::string& v, unsigned int l );
+    Lexeme( Token t, const std::string& v, unsigned int l );
 
-    bool is( Token k ) const;
+    bool is(Token exp);
+
+    std::string toString() const;
   };
 } // basic
   
