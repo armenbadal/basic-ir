63544b3d08dbe2264eab5a60b01857f0f53b941f
Armen Badalian <armenbadal@gmail.com>
2017-05-21 13:02:02 +0400

    Update scanner.hxx

--- src/scanner.hxx
+++ src/scanner.hxx
@@ -2,7 +2,38 @@
 #ifndef SCANNER_HXX
 #define SCANNER_HXX
 
+#include <fstream>
+#include <string>
+#include <map>
+#include <functional>
 
+#include "Lexeme.h"
+
+namespace basic {
+  class Scanner {
+  private:
+    static std::map<const std::string, Token> keywords;
+
+  private:
+    std::ifstream source;
+    char ch;
+
+    unsigned int line = 1;
+
+  public:
+    Scanner( const std::string& filename );
+    ~Scanner();
+
+    Scanner& operator>>( Lexeme& lex );
+
+  private:
+    void sequence( std::function<bool(char)> pred, std::string& res );
+    bool next( Lexeme& lex );
+    bool scanNumber( Lexeme& lex );
+    bool scanText( Lexeme& lex );
+    bool scanIdentifier( Lexeme& lex );
+  };
+} // basic
 
 
 #endif
